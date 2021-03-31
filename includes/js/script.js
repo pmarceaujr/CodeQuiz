bd7ca108a18e0e9bf149e88d718fe7b80b6026b7
Paul Marceau <76534805+pmarceaujr@users.noreply.github.com>
2021-03-30 23:22:53 -0400

    made css changes for background color and text colors.  Added some shadow effects

--- includes/js/script.js
+++ includes/js/script.js
@@ -4,14 +4,16 @@ let gameLost = false;       //Boolean indicating if the game end was due to  win
 let gameScore = 0;          //The player score
 let quizQues = "";          //Quiz question element, where the quetions are written
 let quizChoices = "";       //The elemnet for the various quiz answers
-let timeLeft = 0;          //Time left in the game: defal to 60 seconds
+let timeLeft = 0;           //Time left in the game: defal to 60 seconds
 let timerElm = "";          //The timer element, where the current time left is displayed
 let timerInterval = 0;      //The timerinterval, used to stop the timer if the player asnwers all the questions
+let highScores = [];        //The array to store the high scores, that will write them to local storage
 
 // Create evet listener on the start, home, submit and Scores buttons
 //The start button will start the quiz
 let startBtn = document.querySelector("#startButton");
 startBtn.addEventListener('click', function () {
+    startBtn.style.visibility = 'hidden'
     startTimer();
     loadQuizQues();
 })
@@ -19,26 +21,13 @@ startBtn.addEventListener('click', function () {
 //The submit button will submit the high score to be saved to local storage
 let submitBtn = document.querySelector("#submitButton");
 submitBtn.addEventListener('click', function () {
-    startTimer();
-})
-//The home button will take the user back to the start quiz page
-let homeBtn = document.querySelector("#homeButton");
-homeBtn.addEventListener('click', function () {
-    window.location.href = "http://jsfiddle.net/Xotic750/u5nmt/"; ('./index.html', '_self', false);
-})
-//The scores button will take the user to the high scores page
-let scoresBtn = document.querySelector("#scoresButton");
-scoresBtn.addEventListener('click', function () {
-    window.location.replace('./codegurus.html')
-    //document.location.href = './codegurus.html'
-    //window.open('./codegurus.html', '_self');
+    saveHighScore();
 })
 
 function startTimer() {
     //console.log(`starttimer`)
     timeLeft = 60;
     // Start interval timer
-
     timerElm = document.querySelector("#timer");
     timerInterval = setInterval(function () {
         if (timeLeft < 10) {
@@ -47,14 +36,14 @@ function startTimer() {
         else {
             timerElm.textContent = `0:${timeLeft}`;
         }
-        if (timeLeft === 0) {
+        if (timeLeft <= 0) {
             // Cancel interval
             clearInterval(timerInterval);
+            timerElm.textContent = `0:00`
             gameLost = true;
             gameOver()
         }
         timeLeft--;
-        //loadQuizQues();
     }, 1000);
 }
 
@@ -90,17 +79,15 @@ function answerClicked(event) {
         //if answer correct 
         if (ansElem.textContent === quizQuestions[quesNum][2]) {
             //add to 5 secs to timer add 10 to score, display answer coorect message
-            //alert("yes")
             gameScore += 10;
             timeLeft += 5;
             let msgCenter = document.querySelector('#msgCenter');
             msgCenter.textContent = "CORRECT: You get 10 points and 5 extra seconds."
             let msgFooter = document.querySelector('.hrFooter');
             msgFooter.style.visibility = "visible"
-            //alert(gameScore)
         }
         else {
-            //deduct 1- secs from timer
+            //deduct 10 secs from timer
             timeLeft -= 10;
             let msgCenter = document.querySelector('#msgCenter');
             msgCenter.textContent = "WRONG: You get 0 points and loose 10 seconds."
@@ -110,44 +97,48 @@ function answerClicked(event) {
         }
 
         //Increase the quesNUm so next time through it will load the next question from the array
-        console.log(`q# ${quesNum}`);
         quesNum++
-        console.log(`q# ${quesNum}`);
         loadQuizQues()
     }
 }
 
-
-
-
-
-
 function gameOver() {
     clearInterval(timerInterval);
-    if (gameLost) {
+    let quizSection = document.querySelector('.center-quiz');
+    quizSection.style.display = "none"
+    let hiScoreSection = document.querySelector('.center-hiscore');
+    hiScoreSection.style.display = "unset"
+    if (gameLost || gameScore < 75) {
         //You losr you did not complete all the questions before the timer ran out... do some game loser processing here
-        alert(`you lost`)
-
+        let playerInitials = document.querySelector('#initials');
+        playerInitials.style.display = "none"
+        let playerInitLbl = document.querySelector('.initials');
+        playerInitLbl.style.display = "none"
+        submitBtn.style.display = "none"
+        let youLostMsg = document.querySelector('#highScoreTitle');
+        youLostMsg.textContent = "You Failed The Quiz.  You Should Study More."
     }
     else {
-        //add time to score and do some gam winning porcessing here?
-        //alert(`you win`)
+        //add time to score and do some game winning porcessing here, display Your score
         gameScore = gameScore + timeLeft
-        // alert(gameScore)
-        let quizSection = document.querySelector('.center-quiz');
-        quizSection.style.display = "none"
-        let hiScoreSection = document.querySelector('.center-hiscore');
-        hiScoreSection.style.display = "unset"
         let hiScoreMsg = document.querySelector('#msgHiscore')
         hiScoreMsg.textContent = `Your Score: ${gameScore}`
-
-
-
-
-        ///.center-quiz
-        ///.center-hiscore
     }
 }
+
+function saveHighScore() {
+    //Get the Highscores from localstorage and save them in the highScores array
+    highScores = JSON.parse(localStorage.getItem("highScores"));
+    //Get player Initials and save them to playInitHiScr array and then 
+    //push to HighScores array to save to localstorage
+    let playerInitials = document.querySelector('#initials');
+    let playInitHiScr = [playerInitials.value, gameScore]
+    highScores.push(playInitHiScr);
+    //Save new score and player initials to local storage 
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+
 /*
 //Local storage functions begin here
 function getLocalStorage() {
